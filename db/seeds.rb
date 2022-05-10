45965430bfc8a8e14f7eee5a99fcd851d8c2d698
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-10 12:19:38 -0400

    user seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding users..."
+    User.create(username: "alohaMrHand", first_name: "Liz", :last_name "Dowling", image_url: "https://pbs.twimg.com/profile_images/586694879574654976/FWt4F6FR_400x400.jpg")
+    User.create(username: "joshcasino", first_name: "Josh", :last_name "Casuccio", :image_url "https://imageio.forbes.com/images-forbes/media/2010/04/01/0401_gambling-intro_400x400.jpg?format=jpg&width=1200")
+    User.create(username: "bloodyflymo", first_name: "Sarah", :last_name "Hawkins", :image_url "https://pbs.twimg.com/profile_images/805772835545153536/enb7hBRR_400x400.jpg")
+    User.create(username: "jpegalo", first_name: "Joseph", :last_name "Pegasus", :image_url "https://i.pinimg.com/474x/c5/97/c2/c597c2cd270805d55f4494d3fe87842a.jpg")
 
-# Seed your database here
+puts "✅ Done seeding users!"
+puts "🌱 Seeding activities..."
 
-puts "✅ Done seeding!"
+puts "✅ Done seeding activities!"
+puts "🌱 Seeding chemicals..."
+
+
+puts "✅ Done seeding chemicals!"
