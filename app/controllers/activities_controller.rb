6de1ba7c2d64ac9a9bb77c927a76140235e7bf17
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-12 10:13:00 -0400

    activity controller built and tested

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,5 @@
 class ActivitiesController < Sinatra::Base
     set :default_content_type, 'application/json'
-    
-    get "/activities" do
-        activities = Activity.all
-        activities.to_json
-    end
 
     post "/activities" do
         new_activity = Activity.create(
@@ -17,6 +12,25 @@ class ActivitiesController < Sinatra::Base
             created_at: params[:timestamp]
         )
         new_activity.to_json
+    end
+
+    patch '/activities/:id' do
+        activity = Activity.find(params[:id])
+        activity.update(
+            name: params[:name],
+            notes: params[:notes],
+            duration: params[:duration],
+            location: params[:location],
+            chemical_id: params[:chemical_id]
+        )
+        activity.to_json
+    end
+
+    delete "/activities/:id" do
+        activity = Activity.find(params[:id])
+        
+        activity.destroy
+        activity.to_json
     end
   
   end
